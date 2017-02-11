8246e590f38c0a4defb22481b6cdccd4a5ef1333
Sergio Ortega <sjfox94@gmail.com>
2017-02-11 08:47:48 -0800

    Update project03.asm

--- project03.asm
+++ project03.asm
@@ -17,9 +17,18 @@ intro_msg_2 BYTE "What is your name? ", 0
 intro_msg_3 BYTE "Welcome, ", 0
 instr_msg_1 BYTE "Please enter negative integers between [-100, -1].", 0
 instr_msg_2 BYTE "Enter a non-negative number when you are finished to see the results.", 0
+
+;Receive var
+enumber_msg BYTE "Enter number: ",0
+
+;User inputted var
 username BYTE ?
 username_offset DWORD ?
+uintinput SDWORD ?
 
+;Outro Var
+outro_msg_1 BYTE "Thank you for using the Negative Accumlator, have a fanatastic day ", 0
+outro_msg_2 BYTE "!", 0
 ; (insert variable definitions here)
 
 .code
@@ -45,8 +54,24 @@ call CrLf
 mov edx, OFFSET instr_msg_2
 call WriteString
 call CrLf
+call CrLf
+
+;Receive User input
+mov edx, OFFSET enumber_msg
+call WriteString
+call ReadInt
+mov uintinput, eax
 
 ; (insert executable instructions here)
+;Outro
+call CrLf
+mov edx, OFFSET outro_msg_1
+call WriteString
+;mov edx, OFFSET username
+;call WriteString
+mov edx, OFFSET outro_msg_2
+call WriteString
+call CrLf
 
 	exit	; exit to operating system
 main ENDP
